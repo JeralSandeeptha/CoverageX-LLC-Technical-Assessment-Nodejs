b0b756ed37394456c1db3898484a11129294e502
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-02-22 19:50:58 +0530

    Bug fixing

--- src/api/middlewares/userAuthenticator.ts
+++ src/api/middlewares/userAuthenticator.ts
@@ -1,27 +1,29 @@
-import logger from 'api/config/logger/logger';
+import logger from '../config/logger/logger';
 import dotenv from 'dotenv';
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
-const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+const authenticateToken = (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    
+    return new Promise((resolve, reject) => {
+        const token = req.header('Authorization')?.split(' ')[1];
 
-    if (!token) {
-        logger.error('Access denied. No token provided.');
-        return res.status(401).json({ message: 'Access denied. No token provided.' });
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
-        console.log(decoded);
-
-        next();
-    } catch (error) {
-        logger.error('Invalid token');
-        return res.status(403).json({ message: 'Invalid token.' });
-    }
+        if (!token) {
+            logger.error('Access denied. No token provided.');
+            return resolve(res.status(401).json({ message: 'Access denied. No token provided.' }));
+        }
+    
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET) as jwt.JwtPayload;
+    
+            next();
+        } catch (error) {
+            logger.error('Invalid token');
+            return resolve(res.status(403).json({ message: 'Invalid token.' }));
+        }
+    })
 };
 
 export {
--- src/api/routes/todo.routes.ts
+++ src/api/routes/todo.routes.ts
@@ -1,11 +1,12 @@
+import { authenticateToken } from '../middlewares/userAuthenticator';
 import { createTodo, getTodo, getTodosByUserId, updateTodo } from '../controllers/todo.controller';
 import express from 'express';
 
 const router = express.Router();
 
-router.post('/', createTodo);
-router.get('/:todoId', getTodo);
-router.get('/getTodosByUserId/:userId', getTodosByUserId);
-router.patch('/:todoId', updateTodo);
+router.post('/', authenticateToken, createTodo);
+router.get('/:todoId', authenticateToken, getTodo);
+router.get('/getTodosByUserId/:userId', authenticateToken, getTodosByUserId);
+router.patch('/:todoId', authenticateToken, updateTodo);
 
 export default router;
